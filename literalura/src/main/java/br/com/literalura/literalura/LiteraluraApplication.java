d67d1efb3e61aa4dd4fe490cf5069933ab99ea32
William <pedro.williamsoares2022@gmail.com>
2026-03-01 17:31:28 -0300

    feat(ui): implementar menu interativo e fluxo principal da aplicacao

--- literalura/src/main/java/br/com/literalura/literalura/LiteraluraApplication.java
+++ literalura/src/main/java/br/com/literalura/literalura/LiteraluraApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LiteraluraApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LiteraluraApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(LiteraluraApplication.class, args);
+  }
 }
