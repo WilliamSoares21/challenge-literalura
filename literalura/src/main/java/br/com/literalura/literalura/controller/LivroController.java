26b592c76ff9a85c9cc0c83bd6f89586cee3f20a
William <pedro.williamsoares2022@gmail.com>
2026-03-19 07:59:32 -0300

    feat(controller): implementar LivroController com injeção de LivroService

--- literalura/src/main/java/br/com/literalura/literalura/controller/LivroController.java
+++ literalura/src/main/java/br/com/literalura/literalura/controller/LivroController.java
@@ -1,6 +1,15 @@
 package br.com.literalura.literalura.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import br.com.literalura.literalura.service.LivroService;
+
+@RestController
+@RequestMapping("/livros")
 public class LivroController {
 
-    //Futuramente irei implementar algumas rotas para praticar
+  @Autowired
+  private LivroService livroService;
+
 }
