434dab43f45ed601e5c07f7655c70fa9c477f664
William <pedro.williamsoares2022@gmail.com>
2026-03-19 21:30:05 -0300

    feat(api): adicionar endpoint de busca por titulo com mapeamento para dto

--- literalura/src/main/java/br/com/literalura/literalura/controller/LivroController.java
+++ literalura/src/main/java/br/com/literalura/literalura/controller/LivroController.java
@@ -1,8 +1,15 @@
 package br.com.literalura.literalura.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.literalura.literalura.dto.LivroDTO;
+import br.com.literalura.literalura.mapper.LivroMapper;
+import br.com.literalura.literalura.model.Livro;
 import br.com.literalura.literalura.service.LivroService;
 
 @RestController
@@ -11,5 +18,17 @@ public class LivroController {
 
   @Autowired
   private LivroService livroService;
+
+  @Autowired
+  private LivroMapper mapper;
+
+  @GetMapping
+  public ResponseEntity<LivroDTO> buscarPorTitulo(@RequestParam String titulo) {
+    Livro livro = livroService.buscarESalvarLivroPorTitulo(titulo);
+
+    LivroDTO dto = mapper.converteLivroParaDTO(livro);
+
+    return ResponseEntity.ok(dto);
+  }
 
 }
---
+++ literalura/src/main/java/br/com/literalura/literalura/mapper/LivroMapper.java
@@ -0,0 +1,31 @@
+package br.com.literalura.literalura.mapper;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import br.com.literalura.literalura.dto.AutorDTO;
+import br.com.literalura.literalura.dto.LivroDTO;
+import br.com.literalura.literalura.model.Livro;
+
+@Component
+public class LivroMapper {
+   public LivroDTO converteLivroParaDTO(Livro livro) {
+        // Mapeia o autor da entidade para o DTO
+        AutorDTO autorDTO = null;
+        if (livro.getAutor() != null) {
+            autorDTO = new AutorDTO(
+                livro.getAutor().getNome(),
+                livro.getAutor().getAnoNascimento(),
+                livro.getAutor().getAnoFalecimento()
+            );
+        }
+
+        return new LivroDTO(
+            livro.getTitulo(),
+            autorDTO != null ? List.of(autorDTO) : List.of(),
+            List.of(livro.getIdioma()),
+            livro.getNumeroDownloads()
+        );
+    }
+}
