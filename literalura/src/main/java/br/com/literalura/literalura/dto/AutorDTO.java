0053b0795444dc8beb72ad389d417239eeeb7176
William <pedro.williamsoares2022@gmail.com>
2026-03-19 21:29:57 -0300

    refactor(dto): renomear campos de autor para padrao do dominio

--- literalura/src/main/java/br/com/literalura/literalura/dto/AutorDTO.java
+++ literalura/src/main/java/br/com/literalura/literalura/dto/AutorDTO.java
@@ -1,7 +1,9 @@
 package br.com.literalura.literalura.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record AutorDTO(
-    String name,
-    int birth_year,
-    int death_year) {
+    String nome, 
+    Integer anoNascimento,
+    Integer anoFalecimento) {
 }
