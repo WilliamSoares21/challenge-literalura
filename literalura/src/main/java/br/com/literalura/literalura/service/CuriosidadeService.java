b01481ee74b00903ff0f60e0ad2e92e7b732a6a3
William <pedro.williamsoares2022@gmail.com>
2026-03-20 21:31:07 -0300

    refactor: remover CuriosidadeService substituído por ConsultaGemini

--- literalura/src/main/java/br/com/literalura/literalura/service/CuriosidadeService.java
+++
@@ -1,8 +0,0 @@
-package br.com.literalura.literalura.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class CuriosidadeService {
-  // Depois irei implementar o consumo da API do gemini
-}
